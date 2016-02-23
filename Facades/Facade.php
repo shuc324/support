0fb690c66a6eef8b2e82bc7d0f301992fd3f01b3
shuchao <shuchao@medlinker.net>
2016-02-23 14:41:02 +0800

    dev

---
+++ Facades/Facade.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Shuc324\Support\Facades;
+
+use RuntimeException;
+
+/**
+ * 门面类
+ */
+abstract class Facade
+{
+    const PARAM_LEN_ZE = 0;
+    const PARAM_LEN_ON = 1;
+    const PARAM_LEN_TW = 2;
+    const PARAM_LEN_TH = 3;
+    const PARAM_LEN_FO = 4;
+
+    protected static $app;
+
+    protected static $resolveInstance;
+
+    /**
+     * 解析门面
+     */
+    protected static function resolveFacadeInstance($facade)
+    {
+        if (is_object($facade)) {
+            return $facade;
+        }
+
+        if (isset(static::$resolveInstance[$facade])) {
+            return static::$resolveInstance[$facade];
+        }
+
+        return static::$resolveInstance[$facade] = static::$app[$facade];
+    }
+
+    /**
+     * 设置Application
+     * @param $app
+     * @return mixed
+     */
+    public static function setApplication($app)
+    {
+        return static::$app = $app;
+    }
+
+    /**
+     * 设置门面
+     */
+    public static function setFacade()
+    {
+        throw new RuntimeException("Facade does not implement setFacade method.");
+    }
+
+    /**
+     * 获取门面实例
+     */
+    public static function getFacadeInstance()
+    {
+        return static::resolveFacadeInstance(static::setFacade());
+    }
+
+    /**
+     * 执行具体方法
+     * @param $method
+     * @param $parameters
+     * @return mixed
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        $instance = static::getFacadeInstance();
+        if (empty($instance)) {
+            throw new RuntimeException("A facade has not been set.");
+        }
+        switch (count($parameters)) {
+            case self::PARAM_LEN_ZE:
+                return $instance->$method();
+            case self::PARAM_LEN_ON:
+                return $instance->$method($parameters[self::PARAM_LEN_ZE]);
+            case self::PARAM_LEN_TW:
+                return $instance->$method($parameters[self::PARAM_LEN_ZE], $parameters[self::PARAM_LEN_ON]);
+            case self::PARAM_LEN_TH:
+                return $instance->$method($parameters[self::PARAM_LEN_ZE], $parameters[self::PARAM_LEN_ON], $parameters[self::PARAM_LEN_TW]);
+            case self::PARAM_LEN_FO:
+                return $instance->$method($parameters[self::PARAM_LEN_ZE], $parameters[self::PARAM_LEN_ON], $parameters[self::PARAM_LEN_TW], $parameters[self::PARAM_LEN_TH]);
+            default:
+                return call_user_func_array([$instance, $method], $parameters);
+        }
+    }
+}
