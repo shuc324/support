d7218f28da63138ab2b5380bc8e88268cd8007ec
shuchao <shuchao@medlinker.net>
2016-03-01 10:49:14 +0800

    xml and validator

---
+++ Facades/Facade/Validator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shuc324\Support\Facades\Facade;
+
+use Shuc324\Support\Facades\Facade;
+
+class Validator extends Facade
+{
+    protected static function setFacade()
+    {
+        return "validator";
+    }
+}
---
+++ Facades/Facade/Xml.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shuc324\Support\Facade;
+
+use Shuc324\Support\Facades\Facade;
+
+class Xml extends Facade
+{
+    protected static function setFacade()
+    {
+        return 'xml';
+    }
+}
